bb771268790eb6b39a7b2cb904d75e52901caf73
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-08-07 14:25:33 -0300

    Work in progress

--- handler.py
+++ handler.py
@@ -33,7 +33,7 @@ def rate_limit(resource: str = "", expire: int = 60 * 10):
                 with RateLimit(
                     redis_pool=redis_pool,
                     resource=resource,
-                    client=kwargs["key"],
+                    client=kwargs["message"].chat.id,
                     max_requests=1,
                     expire=expire,
                 ):
@@ -105,7 +105,7 @@ def on_message(update: Update, context: CallbackContext) -> None:
             escape_markdown("".join(caption), version=2),
         ]
 
-        reply(key=chat_id, message=message, text="\n\n".join(messages))
+        reply(message=message, text="\n\n".join(messages))
 
 
 def leaderboard(update: Update, context: CallbackContext) -> None:
