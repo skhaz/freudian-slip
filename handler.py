abf45e5623c52483ef4699f8e55628f3a7b9c06b
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-11-19 11:15:05 -0300

    Work in progress

--- handler.py
+++ handler.py
@@ -1,66 +1,133 @@
+import abc
+import asyncio
 import json
+import logging
 import os
-from queue import Queue
+from typing import Dict
+from typing import Optional
+from typing import TypedDict
 
-from redis import ConnectionPool
-from redis import Redis
-from telegram import Bot
-from telegram import ParseMode
+import aioboto3
 from telegram import Update
-from telegram.ext import CallbackContext
-from telegram.ext import CommandHandler
-from telegram.ext import Dispatcher
-from telegram.ext import Filters
+from telegram.constants import ParseMode
+from telegram.ext import Application
+from telegram.ext import ContextTypes
 from telegram.ext import MessageHandler
-from telegram.utils.helpers import escape_markdown
+from telegram.ext import filters
+from telegram.helpers import escape_markdown
 
-bot = Bot(token=os.environ["TOKEN"])
 
-redis_pool = ConnectionPool.from_url(os.environ["REDIS_DSN"])
-redis = Redis(connection_pool=redis_pool)
+class APIGatewayProxyEventV1(TypedDict):
+    headers: Dict[str, str]
+    body: Optional[str]
+
+
+class Context(metaclass=abc.ABCMeta):
+    pass
+
+
+application = (
+    Application.builder()
+    .token(os.environ.get("TELEGRAM_TOKEN", ""))
+    .updater(None)
+    .build()
+)
+
+boto3 = aioboto3.Session()
+
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 word = os.environ["WORD"]
 
 
-def on_message(update: Update, context: CallbackContext) -> None:
-    message = update.message
+def find_seq(word, message):
+    indices = []
+    word_index = 0
+    current_index = 0
 
+    words = message.split()
+
+    for w in words:
+        if word_index < len(word):
+            if w[0] == word[word_index]:
+                indices.append(current_index)
+                word_index += 1
+            else:
+                indices = []
+                word_index = 0
+                if w[0] == word[word_index]:
+                    indices.append(current_index)
+                    word_index += 1
+        current_index += len(w) + 1
+
+    return indices if word_index == len(word) else []
+
+
+async def main(event: APIGatewayProxyEventV1):
+    body = event["body"]
+    if not body:
+        return
+
+    async with application:
+        await application.process_update(
+            Update.de_json(json.loads(body), application.bot)
+        )
+
+
+def equals(left, right):
+    if not left or not right:
+        return False
+
+    if len(left) != len(right):
+        return False
+
+    for c1, c2 in zip(left, right):
+        if c1 != c2:
+            return False
+
+    return True
+
+
+def telegram(event: APIGatewayProxyEventV1, context: Context):
+    if not equals(
+        event["headers"].get("x-telegram-bot-api-secret-token"),
+        os.environ["SECRET"],
+    ):
+        return {
+            "statusCode": 401,
+        }
+
+    asyncio.get_event_loop().run_until_complete(main(event))
+
+    return {
+        "statusCode": 200,
+    }
+
+
+async def on_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = update.message
     if not message:
         return
 
-    text = message.text
+    user = message.from_user
+    if not user:
+        return
 
+    text = message.text
     if not text:
         return
 
-    escaped_text = escape_markdown(text, version=2)
-    letters = list(word)
-    cursor = 0
-    indexes = []
-    begin_of_word = True
-    in_sequence = False
+    # escaped_text = escape_markdown(text, version=2)
+    text = escape_markdown(text, version=2)
 
-    for index, char in enumerate(escaped_text):
-        if char == " ":
-            begin_of_word = True
-            continue
-        if begin_of_word:
-            begin_of_word = False
-            if char == letters[cursor]:
-                indexes.append(index)
-                cursor += 1
-                in_sequence = True
-                if cursor == len(letters):
-                    break
-                continue
-            in_sequence = False
-            cursor = 0
-            indexes = []
+    indexes = find_seq(word, text)
 
     is_same_length = len(indexes) == len(word)
 
-    if is_same_length and in_sequence:
-        letters = [char for char in escaped_text]
+    if is_same_length:
+        letters = [char for char in text]
         for i, index in enumerate(indexes):
             letters.insert(index + i, "*")
 
@@ -68,17 +135,10 @@ def on_message(update: Update, context: CallbackContext) -> None:
         for i, index in enumerate(indexes):
             letters.insert((index + 2) + i, "*")
 
-        user_id = message.from_user.id
+        count = 1
+        count_by_author = 3
+        # user_id = message.from_user.id
         user = message.from_user.name
-        one_year_in_seconds = 60 * 60 * 24 * 365
-
-        pipeline = redis.pipeline(transaction=False)
-        pipeline.incr(word)
-        pipeline.incr(f"{word}:count:{user_id}")
-        pipeline.set(f"{word}:user:{user_id}", user)
-        pipeline.expire(f"{word}:count:{user_id}", one_year_in_seconds)
-        pipeline.expire(f"{word}:user:{user_id}", one_year_in_seconds)
-        count, count_by_author, *_ = pipeline.execute()
 
         caption = [
             f"Hidden {word} detected! {count} have been discovered so far. "
@@ -90,52 +150,12 @@ def on_message(update: Update, context: CallbackContext) -> None:
             escape_markdown("".join(caption), version=2),
         ]
 
-        message.reply_text("\n\n".join(messages), parse_mode=ParseMode.MARKDOWN_V2)
+        await message.reply_text(
+            "\n\n".join(messages), parse_mode=ParseMode.MARKDOWN_V2
+        )
 
 
-def leaderboard(update: Update, context: CallbackContext) -> None:
-    message = update.message
-
-    if not message:
-        return
-
-    get_count = (
-        lambda key: int(value.decode()) if (value := redis.get(key)) is not None else 0
-    )
-
-    users = {
-        key.decode().split(":")[2]: get_count(key)
-        for key in redis.scan_iter(f"{word}:count:*")
-    }
-
-    sorted_users = sorted(users.items(), key=lambda x: x[1], reverse=True)[:10]
-
-    get_username = (
-        lambda key: username.decode()
-        if (username := redis.get(key)) is not None
-        else None
-    )
-
-    mention = (
-        lambda uid: f"[{escape_markdown(get_username(f'{word}:user:{uid}'), version=2)}](tg://user?id={uid})"
-    )  # noqa
-
-    users = [
-        rf"\* {mention(user[0])} worshipped the {word} {user[1]} times"  # noqa
-        for user in sorted_users
-    ]
-
-    message.reply_text("\n".join(users), parse_mode=ParseMode.MARKDOWN_V2)
-
-
-dispatcher = Dispatcher(bot=bot, update_queue=Queue())
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, on_message))
-dispatcher.add_handler(CommandHandler("leaderboard", leaderboard))
-
-
-def telegram(event, context):
-    dispatcher.process_update(Update.de_json(json.loads(event["body"]), bot))
-
-    return {
-        "statusCode": 200,
-    }
+application.add_handler(
+    MessageHandler(filters.Filters.text & ~filters.Filters.command, on_message)
+)
+# application.add_handler(CommandHandler("leaderboard", leaderboard))
