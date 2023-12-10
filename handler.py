3336f318aeaf2cf38fc9011531c5262a18f13b8e
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-12-10 10:02:43 -0300

    Work in progress

--- handler.py
+++ handler.py
@@ -4,6 +4,7 @@ import json
 import logging
 import os
 import re
+from enum import Enum
 from typing import Dict
 from typing import List
 from typing import Optional
@@ -30,12 +31,7 @@ class Context(metaclass=abc.ABCMeta):
     pass
 
 
-application = (
-    Application.builder()
-    .token(os.environ.get("TELEGRAM_TOKEN", ""))
-    .updater(None)
-    .build()
-)
+application = Application.builder().token(os.environ.get("TELEGRAM_TOKEN", "")).updater(None).build()
 
 boto3 = aioboto3.Session()
 
@@ -87,9 +83,7 @@ async def main(event: APIGatewayProxyEventV1):
         return
 
     async with application:
-        await application.process_update(
-            Update.de_json(json.loads(body), application.bot)
-        )
+        await application.process_update(Update.de_json(json.loads(body), application.bot))
 
 
 def equals(left, right):
@@ -122,6 +116,41 @@ def telegram(event: APIGatewayProxyEventV1, context: Context):
     }
 
 
+class Kind(Enum):
+    WORD = "word"
+    NUMBER = "number"
+
+
+async def store_and_compute(key: dict[str, str], name: str, kind: Kind):
+    async with boto3.resource("dynamodb") as dynamodb:
+        table = await dynamodb.Table(os.environ[f"USER_{kind.value.upper()}_TABLE"])
+        global_table = await dynamodb.Table(os.environ[f"GLOBAL_{kind.value.upper()}_TABLE"])
+
+        [response, global_response] = await asyncio.gather(
+            table.update_item(
+                Key=key,
+                UpdateExpression="SET score = if_not_exists(score, :start) + :inc, #n = :name",
+                ExpressionAttributeNames={"#n": "name"},
+                ExpressionAttributeValues={
+                    ":start": 0,
+                    ":inc": 1,
+                    ":name": name,
+                },
+                ReturnValues="UPDATED_NEW",
+            ),
+            global_table.update_item(
+                Key={"id": "global"},
+                UpdateExpression="SET score = if_not_exists(score, :start) + :inc",
+                ExpressionAttributeValues={":start": 0, ":inc": 1},
+                ReturnValues="UPDATED_NEW",
+            ),
+        )
+
+        score = response["Attributes"]["score"]
+        global_score = global_response["Attributes"]["score"]
+        return score, global_score
+
+
 async def on_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message
     if not message:
@@ -139,6 +168,8 @@ async def on_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
 
     indexes = find_sequence(word, text)
 
+    mention = f"[{user.username}](tg://user?id={user.id})"
+
     is_same_length = len(indexes) == len(word)
 
     if is_same_length:
@@ -152,57 +183,34 @@ async def on_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
 
         key = {"id": str(user.id)}
         name = user.username or user.first_name
+        score, global_score = await store_and_compute(key, name, Kind.WORD)
 
-        async with boto3.resource("dynamodb") as dynamodb:
-            table = await dynamodb.Table(os.environ["USER_WORD_TABLE"])
-            global_table = await dynamodb.Table(os.environ["GLOBAL_WORD_TABLE"])
+        caption = [
+            f"Hidden {word.upper()} detected\!\n",
+            f"{mention} has already worshiped the {word} {score} times\.\n",
+            f"{global_score} have been discovered so far\.\n",
+        ]
 
-            [response, global_response] = await asyncio.gather(
-                table.update_item(
-                    Key=key,
-                    UpdateExpression="SET score = if_not_exists(score, :start) + :inc, #n = :name",
-                    ExpressionAttributeNames={"#n": "name"},
-                    ExpressionAttributeValues={
-                        ":start": 0,
-                        ":inc": 1,
-                        ":name": name,
-                    },
-                    ReturnValues="UPDATED_NEW",
-                ),
-                global_table.update_item(
-                    Key={"id": "global"},
-                    UpdateExpression="SET score = if_not_exists(score, :start) + :inc",
-                    ExpressionAttributeValues={":start": 0, ":inc": 1},
-                    ReturnValues="UPDATED_NEW",
-                ),
-            )
+        messages = [
+            "".join(letters),
+            "".join(caption),
+        ]
 
-            score = response["Attributes"]["score"]
-            global_score = global_response["Attributes"]["score"]
-
-            mention = f"[{user.username}](tg://user?id={user.id})"
-
-            caption = [
-                f"Hidden {word.upper()} detected\!\n"
-                f"{mention} has already worshiped the {word} {score} times\.\n",
-                f"{global_score} have been discovered so far\.\n",
-            ]
-
-            messages = [
-                "".join(letters),
-                "".join(caption),
-            ]
-
-            await message.reply_text(
-                "\n\n".join(messages), parse_mode=ParseMode.MARKDOWN_V2
-            )
+        await message.reply_text("\n\n".join(messages), parse_mode=ParseMode.MARKDOWN_V2)
 
     numbers, total = find_numbers(text)
     if total == number:
-        await message.reply_text(f"{' + '.join(str(i) for i in numbers)} = {total}")
+        score, global_score = await store_and_compute(key, name, Kind.NUMBER)
+
+        messages = [
+            f"{' + '.join(str(i) for i in numbers)} = {total}",
+            f"{mention} has already made {number} {score} times\!",
+        ]
+
+        await message.reply_text("\n\n".join(messages), parse_mode=ParseMode.MARKDOWN_V2)
 
 
-async def leaderboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def on_leaderboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message = update.message
     if not message:
         return
@@ -226,11 +234,11 @@ async def leaderboard(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
         top_users = [
             rf"\* [{item['name']}](tg://user?id={item['id']}) has worshipped the {word.upper()} {int(item['score'])} times"
-            for item in sorted(items, key=lambda i: int(i["score"]), reverse=True)[:10]  # fmt: skip
+            for item in sorted(items, key=lambda i: int(i["score"]), reverse=True)[:10]
         ]
 
         await message.reply_text("\n".join(top_users), parse_mode=ParseMode.MARKDOWN_V2)
 
 
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, on_message))
-application.add_handler(CommandHandler("leaderboard", leaderboard))
+application.add_handler(CommandHandler("leaderboard", on_leaderboard))
